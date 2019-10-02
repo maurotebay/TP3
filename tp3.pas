dafeb479738849f79fca3c80a80f531fe4f65cc7
urielman <urielman99@gmail.com>
2019-10-02 18:24:58 -0300

    menu base

--- tp3.pas
+++ tp3.pas
@@ -1,4 +1,62 @@
 
 {Comision 105 - Schlieper Tadeo / Tebay Mauro / Man Uriel}
-program tp2;
+program tp3;
 uses Crt;
+
+procedure OpcionBancos;
+begin
+  
+end;
+
+procedure OpcionABM;
+begin
+  
+end;
+
+procedure OpcionUsuarios;
+begin
+  
+end;
+
+procedure Menu;
+var
+    opcion: integer;
+begin
+    ClrScr;
+    WriteLn('   MENU');
+    WriteLn('1) Bancos');
+    WriteLn('2) ABM de Comercios Adheridos');
+    WriteLn('3) Usuarios');
+    WriteLn('4) Fin');
+    repeat
+        Write('Seleccione ingresando una de las opciones: ');
+        ReadLn(opcion);
+    until (opcion >= 1) AND (opcion <= 4);
+    case opcion of
+        1: begin
+            ClrScr;
+            OpcionBancos;
+            ReadKey;
+            Menu;
+        end;
+        2: begin
+            ClrScr;
+            OpcionABM;
+            ReadKey;
+            Menu;
+        end;
+        3: begin
+            ClrScr;
+            OpcionUsuarios;
+            ReadKey;
+            Menu;
+        end;
+        4: WriteLn('Fin del menu.');
+    end
+end;
+
+begin
+  Menu;
+  Write('Presione cualquier tecla para salir del programa');
+  ReadKey;
+end.
