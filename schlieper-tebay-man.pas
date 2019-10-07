5c33ee9c8f5296d43fbd72eb70d674166644a4ff
urielman <urielman99@gmail.com>
2019-10-06 21:19:36 -0300

    intento fallido de manejo de archivos

--- schlieper-tebay-man.pas
+++ schlieper-tebay-man.pas
@@ -3,9 +3,39 @@
 program tp3;
 uses Crt;
 
+{Esta opci?n listar? primero todas las entidades bancarias que ya est?n cargadas en el sistema en bancos.dat, y permitir? ingresar nuevos bancos. Por cada nuevo ingreso se deber? registrar el c?digo del banco y el nombre de la entidad.}
 procedure OpcionBancos;
+type        
+    Banco = record
+        codigoBanco: integer; {(c?digo de banco)}
+        nombreBanco: string [30];
+    end;
+var
+    archivoBancos: file of Banco;
+    nuevoBanco: Banco;
+    opcion: string;
 begin
-  
+    assign(archivoBancos, 'bancos.dat');
+    while not eof(archivoBancos) do
+    begin
+        read(archivoBancos, nuevoBanco);
+        writeln('Nombre: ', nuevoBanco.nombreBanco);
+        writeln('Codigo: ',  nuevoBanco.codigoBanco);
+    end;
+    
+    repeat
+        WriteLn('Desea ingresar un nuevo banco? (si / no)');
+        ReadLn(opcion);
+    until (opcion = 'si') OR (opcion = 'no');
+    if opcion = 'si' then 
+        begin
+            WriteLn('Ingrese el nombre del banco');
+            ReadLn(nuevoBanco.nombreBanco);
+            WriteLn('Ingrese el codigo del banco');
+            ReadLn(nuevoBanco.codigoBanco);
+            Write(archivoBancos, nuevoBanco);
+            close(archivoBancos);
+        end;
 end;
 
 procedure OpcionABM;
