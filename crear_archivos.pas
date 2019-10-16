7546516eb229e609e4c8fb2fb717eca248ea6e21
Tadeo Schlieper <tadeo.sch@hotmail.com>
2019-10-16 20:48:11 -0300

    los archivos creados ahora tienen los datos conectados entre si

--- crear_archivos.pas
+++ crear_archivos.pas
@@ -6,7 +6,7 @@ const
     RUTA_ARCHIVOS = 'C:\ayed\tp3\';
 
 type
-    Usuario = record
+    usuario = record
         dni: string[8];
         contrasena: integer;
         ape_nom: string[30];
@@ -32,23 +32,29 @@ type
         cod_com: integer;
         dni_otro_usuario: string[8];
     end;
-    Comercio = record
+    comercio = record
         cod_com: integer;
         nombre: string[30];
         cuit: string[12];
         estado: boolean;
     end;
-    Banco = record
+    banco = record
         cod_ban: integer; {(codigo de banco)}
         nombre: string[30];
     end;
 
+var
+    archivoBancos: file of banco;
+    archivoComercios: file of comercio;
+    archivoCuentas: file of cuentaVirtual;
+    archivoUsuarios: file of usuario;
+    archivoMovimientos: file of movimiento;
+
 procedure Bancos;
 var
-    archivoBancos: file of Banco;
     nuevoBanco: Banco;
 begin
-    assign(archivoBancos, RUTA_ARCHIVOS + 'Bancos.dat');
+    assign(archivoBancos, RUTA_ARCHIVOS + 'bancos.dat');
     rewrite(archivoBancos);
     
     nuevoBanco.nombre := 'Banco Patagonia';
@@ -68,10 +74,9 @@ end;
 
 procedure Comercios;
 var
-    archivoComercios:file of Comercio;
     nuevoComercio: Comercio;
 begin
-    assign(archivoComercios, RUTA_ARCHIVOS + 'Comercios.dat');
+    assign(archivoComercios, RUTA_ARCHIVOS + 'comercios.dat');
     rewrite(archivoComercios);
 
     nuevoComercio.cod_com:=0;
@@ -97,19 +102,28 @@ end;
 
 procedure CuentasVirtuales;
 var
-    archivoCuentas: file of cuentaVirtual;
     newCuenta: cuentaVirtual;
 begin
     assign(archivoCuentas, RUTA_ARCHIVOS + 'cuentas-virtuales.dat');
     rewrite(archivoCuentas);
 
-    newCuenta.dni:='21568468';
-    newCuenta.saldo_billetera := 420.65;
-    newCuenta.cuenta_virtual[1].cod_ban:=25635125;
+    newCuenta.dni:='11111111';
+    newCuenta.saldo_billetera := 500.65;
+
+    newCuenta.cuenta_virtual[1].cod_ban:=12345678;
+    newCuenta.cuenta_virtual[1].tipo_tar:='C';
+    newCuenta.cuenta_virtual[1].saldo_x_tarjeta:=20.0;
+
+    write(archivoCuentas, newCuenta);
+
+    newCuenta.dni:='12345678';
+    newCuenta.saldo_billetera := 1000;
+
+    newCuenta.cuenta_virtual[1].cod_ban:=87654321;
     newCuenta.cuenta_virtual[1].tipo_tar:='D';
     newCuenta.cuenta_virtual[1].saldo_x_tarjeta:=565.29;
 
-    newCuenta.cuenta_virtual[2].cod_ban:=25566674;
+    newCuenta.cuenta_virtual[2].cod_ban:=87654321;
     newCuenta.cuenta_virtual[2].tipo_tar:='C';
     newCuenta.cuenta_virtual[2].saldo_x_tarjeta:=5565.29;
 
@@ -120,21 +134,20 @@ end;
 
 procedure Usuarios;
 var
-    archivoUsuarios:file of usuario;
     newUsuario:usuario;
 begin
     assign(archivoUsuarios, RUTA_ARCHIVOS + 'usuarios.dat');
     rewrite(archivoUsuarios);
 
-    newUsuario.dni:='44556699';
-    newUsuario.contrasena:=12345;
+    newUsuario.dni:='12345678';
+    newUsuario.contrasena:=1234;
     newUsuario.ape_nom:='Juan Perez';
     newUsuario.mail:='juanperez@tumail.com';
 
     write(archivoUsuarios, newUsuario);
 
-    newUsuario.dni:='56832145';
-    newUsuario.contrasena:=54625;
+    newUsuario.dni:='11111111';
+    newUsuario.contrasena:=1111;
     newUsuario.ape_nom:='Jose Gomez';
     newUsuario.mail:='pepegomez@tumail.com';
 
@@ -145,23 +158,33 @@ end;
 
 procedure Movimientos;
 var
-    archivoMovimientos:file of movimiento;
-    newMovimiento:movimiento;
+    newMovimiento: movimiento;
 begin
     assign(archivoMovimientos, RUTA_ARCHIVOS + 'movimientos.dat');
     rewrite(archivoMovimientos);
 
     newMovimiento.dni:='12345678';
-    newMovimiento.cod_ban:=12546;
+    newMovimiento.cod_ban:=87654321;
     newMovimiento.tipo_tar:='D';
     newMovimiento.importe:=256.12;
     newMovimiento.tipo_movi:='C';
     newMovimiento.dia:=17;
     newMovimiento.mes:=7;
     newMovimiento.ano:=2017;
-    newMovimiento.cod_com:=2525;
-    newMovimiento.dni_otro_usuario:='32165487';
+    newMovimiento.cod_com:=0;
+    newMovimiento.dni_otro_usuario:='';
+    write(archivoMovimientos, newMovimiento);
 
+    newMovimiento.dni:='12345678';
+    newMovimiento.cod_ban:=0;
+    newMovimiento.tipo_tar:=#0;
+    newMovimiento.importe:=500.0;
+    newMovimiento.tipo_movi:='E';
+    newMovimiento.dia:=18;
+    newMovimiento.mes:=7;
+    newMovimiento.ano:=2017;
+    newMovimiento.cod_com:=0;
+    newMovimiento.dni_otro_usuario:='11111111';
     write(archivoMovimientos, newMovimiento);
 
     close(archivoMovimientos);
@@ -171,7 +194,7 @@ begin
     WriteLn('Por favor, asegurate que exista la carpeta "' + RUTA_ARCHIVOS + '" antes de continuar.');
     WriteLn('De lo contrario se cerrara el programa sin confirmacion.');
     WriteLn();
-    Write('Pulse cualquier tecla para continuar y crear los archivos...');
+    Write('Pulse cualquier tecla para continuar y crear los archivos (si ya existen se sobreescribiran)...');
     ReadKey;
     ClrScr;
     WriteLn('Creando archivos...');
